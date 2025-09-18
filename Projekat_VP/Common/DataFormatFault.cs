31f62445e88ea6e60dec3173e737eb923555d75e
Marijana49 <marijanazaric8@gmail.com>
2025-09-18 13:11:36 +0200

    DataFormatFault, ValidationFault

---
+++ Projekat_VP/Common/DataFormatFault.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class DataFormatFault
+    {
+        string message;
+
+        public DataFormatFault(string message)
+        {
+            this.message = message;
+        }
+
+        [DataMember]
+        public string Message { get => message; set => message = value; }
+    }
+}
---
+++ Projekat_VP/Common/ValidationFault.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class ValidationFault
+    {
+        string message;
+
+        public ValidationFault(string message)
+        {
+            this.message = message;
+        }
+
+        [DataMember]
+        public string Message { get => message; set => message = value; }
+    }
+}
