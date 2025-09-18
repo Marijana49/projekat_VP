8bfab169a8d5cedddc300748fa0bd8d5782cea36
Marijana49 <marijanazaric8@gmail.com>
2025-09-18 12:00:36 +0200

    konfiguracija klijenta

--- Projekat_VP/Client/Program.cs
+++ Projekat_VP/Client/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Client
+﻿using Common;
+using System.ServiceModel;
+
+namespace Client
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            ChannelFactory<ISensorContract> factory = new ChannelFactory<ISensorContract>("ServiceContract");
         }
     }
 }
