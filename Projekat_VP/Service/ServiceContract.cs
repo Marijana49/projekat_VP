5d95bff0de583918694591aa2df097c688d116ae
Marijana49 <marijanazaric8@gmail.com>
2025-09-28 20:11:41 +0200

    radi!!

--- Projekat_VP/Service/ServiceContract.cs
+++ Projekat_VP/Service/ServiceContract.cs
@@ -24,25 +24,42 @@ namespace Service
         private double coThreshold = double.Parse(ConfigurationManager.AppSettings["CO_threshold"]);
         private double pThreshold = double.Parse(ConfigurationManager.AppSettings["P_threshold"]);
 
+        private StreamWriter sessionWriter;
+        private StreamWriter rejectsWriter;
 
-        private static string sessionFilePath;
-        private static string rejectsFilePath;
-        private static StreamWriter sessionWriter;
-        private static StreamWriter rejectsWriter;
-
-        public void EndSession()
+        public ServiceContract()
         {
-            Console.WriteLine("Transmission ended.");
+            OnTransferStarted += (s, e) => Console.WriteLine("Event: Transfer has started...");
+            OnSampleReceived += (s, sample) => Console.WriteLine($"Event: Sample received V={sample.volume}, CO={sample.co}, NO2={sample.no2}, P={sample.pressure}");
+            OnTransferCompleted += (s, e) => Console.WriteLine("Event: Transfer has completed.");
+            OnWarningRaised += (s, msg) => Console.WriteLine("Event: Warning: " + msg);
+        }
 
-            sessionWriter?.Close();
-            rejectsWriter?.Close();
+        public void StartSession(string meta)
+        {
+            Console.WriteLine("Transmission in progress...");
+            sampleCount = 0;
+            volumeSum = 0;
+
+            OnTransferStarted?.Invoke(this, EventArgs.Empty);
+
+            sessionWriter = new StreamWriter(new FileStream("measurements_session.csv", FileMode.Append, FileAccess.Write, FileShare.Read));
+            rejectsWriter = new StreamWriter(new FileStream("rejects.csv", FileMode.Append, FileAccess.Write, FileShare.Read));
+
+            sessionWriter.WriteLine("Volume,CO,NO2,Pressure,DateTime");
+            sessionWriter.Flush();
+
+            if (new FileInfo("rejects.csv").Length == 0)
+            {
+                rejectsWriter.WriteLine("Volume,CO,NO2,Pressure,DateTime,Reason");
+                rejectsWriter.Flush();
+            }
         }
 
         public void PushSample(SensorSample sample)
         {
             sampleCount++;
             volumeSum += sample.volume;
-
             double avg = volumeSum / sampleCount;
 
             OnSampleReceived?.Invoke(this, sample);
@@ -56,12 +73,11 @@ namespace Service
 
             //we can do this for others????
             if (Math.Abs(sample.volume - avg) > 0.25 * avg)
-                OnWarningRaised?.Invoke(this, $"Volume varies  25% around the average! ({sample.volume}, avg={avg:F2})");
+                OnWarningRaised?.Invoke(this, $"Volume varies 25% around the average! ({sample.volume}, avg={avg:F2})");
 
             try
             {
                 Validation(sample);
-
                 sessionWriter.WriteLine($"{sample.volume},{sample.co},{sample.no2},{sample.pressure},{sample.dateTime}");
                 sessionWriter.Flush();
             }
@@ -72,29 +88,12 @@ namespace Service
             }
         }
 
-        public void StartSession(string meta)
+        public void EndSession()
         {
-            Console.WriteLine("Transmission in progress...");
-
-            sampleCount = 0;
-            volumeSum = 0;
-
-            OnTransferStarted?.Invoke(this, EventArgs.Empty);
-
-            sessionFilePath = "measurements_session.csv";
-            rejectsFilePath = "rejects.csv";
-
-            sessionWriter = new StreamWriter(new FileStream(sessionFilePath, FileMode.Append, FileAccess.Write, FileShare.Read));
-            rejectsWriter = new StreamWriter(new FileStream(rejectsFilePath, FileMode.Append, FileAccess.Write, FileShare.Read));
-
-            sessionWriter.WriteLine("Volume,CO,NO2,Pressure,DateTime");
-            sessionWriter.Flush();
-
-            if (new FileInfo(rejectsFilePath).Length == 0)
-            {
-                rejectsWriter.WriteLine("Volume,CO,NO2,Pressure,DateTime,Reason");
-                rejectsWriter.Flush();
-            }
+            Console.WriteLine("Transmission ended.");
+            sessionWriter?.Close();
+            rejectsWriter?.Close();
+            OnTransferCompleted?.Invoke(this, EventArgs.Empty);
         }
 
         public void Validation(SensorSample ss)
